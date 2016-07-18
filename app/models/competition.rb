832f65ffd6bf5e367989635f29b1bba98bc86c8e
Michael Wenger <michaelwenger27@gmail.com>
2016-07-17 20:55:11 -0400

    Every competition has a type selected at random

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,9 +1,14 @@
 class Competition < ApplicationRecord
-  before_create :set_competition_type
+  COMPETITION_TYPES = %w(strength wit senses agility)
+  before_validation :set_competition_type, on: :create
+
+  validates :challenger, presence: true, numericality: { only_integer: true }
+  validates :challenged, presence: true, numericality: { only_integer: true }
+  validates :competition_type, presence: true
 
   private
 
   def set_competition_type
-    send("competition_type=", ["Strength"])
+    send("competition_type=", COMPETITION_TYPES.sample)
   end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -2,5 +2,22 @@ require "rails_helper"
 
 RSpec.describe Competition, type: :model do
   describe "validations" do
+    it do
+      is_expected.to validate_numericality_of(:challenger).only_integer
+    end
+
+    it do
+      is_expected.to validate_numericality_of(:challenged).only_integer
+    end
+  end
+
+  describe "#before_create" do
+    it "randomly assigns one of the default competition_types" do
+      5.times { create(:competition, competition_type: nil) }
+
+      competition_types = Competition.all.pluck(:competition_type)
+      expect(competition_types.uniq.length).to be > 1
+      expect(competition_types - Competition::COMPETITION_TYPES).to eq []
+    end
   end
 end
